e33a562ba6b0711c94a2b241b7c8627e5e93bcae
Toby Inkster <tobyink@cpan.org>
2010-05-26 20:00:52 +0000

    Import of TOBYINK/Task-HTML5-0.001 from CPAN.
    gitpan-cpan-distribution: Task-HTML5
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         TOBYINK/Task-HTML5-0.001.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/Task/HTML5.pm
@@ -0,0 +1,44 @@
+package Task::HTML5;
+our $VERSION = '0.001';
+1;
+__END__
+=head1 NAME
+
+Task::HTML5 - install modules for handling HTML5
+
+=head1 VERSION
+
+0.001
+
+=head1 DESCRIPTION
+
+This Task installs the following:
+
+=over 4
+
+=item * L<HTML::HTML5::Parser>
+
+=item * L<HTML::HTML5::Sanity>
+
+=item * L<HTML::HTML5::Writer>
+
+=item * L<XML::LibXML>
+
+=back
+
+=head1 SEE ALSO
+
+L<Task>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT
+
+Copyright 2010 Toby Inkster
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
